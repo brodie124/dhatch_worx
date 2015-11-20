e427b8f6c4a9e40720a3bd386cc2d8e25cac0ad7
Brodie <brodie.pestell7@gmail.com>
2015-11-20 19:25:38 +0000

    Fixed the video plugin

--- assets/js/theme-video.js
+++ assets/js/theme-video.js
@@ -3,54 +3,92 @@ jQuery(document).ready(function() {
 	// Check for mobile device
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		jQuery('#video, .video-btn-play, .video-controls').remove();
-	}
-	// No mobile device detected
-	else {
-
+	} else {
+        // No mobile device detected
 		// Initialize Background Video Plugin
 		jQuery(function(){
 			jQuery('#video').mb_YTPlayer();
 		});
 
-		jQuery('.video-controls').hide();
+        var $video = jQuery("#video");
 
 		// Start Video
-		jQuery('#video').on('YTPStart', function() {
-			jQuery('#home .background-overlay, #home .wrapper, .video-btn-play, header.navbar').fadeOut(800);
-			jQuery('header.navbar').removeClass('show');
-			jQuery('.video-controls').fadeIn(1200);
-			jQuery('.video-controls .video-btn-pause').show();
-			jQuery('.video-controls .video-btn-play').hide();
+		$video.on('YTPStart', function() {
+            setNavbarVisible(false);
 			jQuery('.mb_YTVPBar').css('visibility', 'visible');
 		});
 
 		// Pause Video
-		jQuery('#video').on('YTPPause', function() {
+        $video.on('YTPPause', function() {
+            setNavbarVisible(true);
 			jQuery('#video').trigger('YTPEnd');
-			// jQuery('.video-controls').fadeIn(800);
-			// jQuery('#home .background-overlay, #home .wrapper, header.navbar').fadeIn(1200);
-			// jQuery('header.navbar').addClass('show');
-			// jQuery('.video-controls .video-btn-pause').hide();
-			// jQuery('.video-controls .video-btn-play').show();
 		});
 
 		// End of Video
-		jQuery("#video").on("YTPEndPlaylist", function() {
-			console.log("YTPEndPlaylist");
-			jQuery('.video-controls').fadeOut(800);
-			jQuery('#home .background-overlay, #home .wrapper, .video-btn-play, header.navbar').fadeIn(1200);
-			jQuery('.mb_YTVPBar').css('visibility', 'hidden');
+        $video.on("YTPEndPlaylist", function() {
+            setNavbarVisible(true);
+
 		});
 
 		// Previous / Next Video
 		jQuery('.video-btn-prev, .video-btn-next').click(function() {
-			jQuery('#home .background-overlay, #home .wrapper, .video-btn-play').hide();
+			setNavbarVisible(false);
 		});
 
 		// Toggle Volume
 		jQuery('.video-btn-volume').click(function() {
 			jQuery(this).find('i').toggleClass('fa-volume-up fa-volume-off');
 		});
+
+
+
+
+
+
+
+
+
+
+
+        //SET VISIBILITY OF NAVIGATION BAR
+        function setNavbarVisible(state) {
+            var $navbar = jQuery('#home .background-overlay, #home .wrapper, header.navbar');
+            if(state) {
+
+                //MAKE VISIBLE
+                $navbar.fadeIn(1200);
+                jQuery('header.navbar').addClass('show');
+
+            } else {
+
+                //MAKE INVISIBLE
+                $navbar.fadeOut(800);
+                jQuery('header.navbar').removeClass('show');
+
+            }
+
+            setVideoControlsVisible(!state);
+        }
+
+        function setVideoControlsVisible(state) {
+            var $btnPause = jQuery('.video-controls .video-btn-pause');
+            var $btnPlay = jQuery('.video-controls .video-btn-play');
+            var $btnVolume = jQuery('.video-controls .video-btn-volume');
+
+            if(state) {
+                jQuery('.video-controls').fadeIn(1200);
+                jQuery('.mb_YTVPProgress').fadeIn(1200);
+                $btnPause.show();
+                $btnVolume.show();
+                $btnPlay.hide();
+            } else {
+                jQuery('.video-controls').fadeIn(1200);
+                jQuery('.mb_YTVPProgress').fadeOut(1200);
+                $btnPause.hide();
+                $btnVolume.hide();
+                $btnPlay.show();
+            }
+        }
 
 	}
 
